474cf9427cd7302fde46518f844f28fd0dd2e801
puggybumper <42521168+puggybumper@users.noreply.github.com>
2020-02-01 15:26:23 -0500

    make sheep into clouds when they leave the board

--- Assets/Sheep.cs
+++ Assets/Sheep.cs
@@ -7,8 +7,14 @@ public class Sheep : MonoBehaviour
     public float RandomMovementSpeed = 1.0f;
     public float RandomMovementChange = 1.0f;
 
+    public GameObject cloudTemplate;
+
     public void Die()
     {
+        // create a new object where sheep used to be
+        GameObject cloud = Instantiate(cloudTemplate);
+        cloud.transform.SetPositionAndRotation(transform.position, transform.rotation);
+
         Destroy(this.transform.root.gameObject);
     }
 
