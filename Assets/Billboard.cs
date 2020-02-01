d27a43bcac85be12bfaa8667a895438c9f729729
mklingen <mklingen89@gmail.com>
2020-01-31 22:29:02 -0500

    commit first demo.

---
+++ Assets/Billboard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera _camera;
+    private Vector3 _relPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+        _relPos = transform.localPosition;
+        GetComponentInChildren<SpriteRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        GetComponentInChildren<SpriteRenderer>().receiveShadows = true;
+    }
+
+
+    void Snap()
+    {
+        var proj = _camera.WorldToScreenPoint(transform.parent.position + _relPos);
+        var intProj = new Vector3(Mathf.FloorToInt(proj.x), Mathf.FloorToInt(proj.y), proj.z);
+        transform.position = _camera.ScreenToWorldPoint(intProj);
+    }
+
+    private void OnPreRender()
+    {
+        Snap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Snap();
+        transform.rotation = _camera.transform.rotation;
+    }
+}
---
+++ Assets/Farmer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farmer : MonoBehaviour
+{
+    private Camera _camera = null;
+    private Vector3 _velocity = new Vector3(0, 0, 0);
+    public float Friction = 0.9f;
+    public float Speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 fwd =  _camera.transform.forward;
+        fwd.y = 0.0f;
+        fwd.Normalize();
+        Vector3 right =_camera.transform.right;
+        right.y = 0.0f;
+        right.Normalize();
+        
+        _velocity += (fwd * Input.GetAxis("Vertical") + right * Input.GetAxis("Horizontal")) * Speed;
+        transform.position += _velocity * Time.deltaTime;
+        _velocity *= Friction;
+    }
+}
