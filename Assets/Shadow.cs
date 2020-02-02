ba236a399b369f21b0f68a4b5892d47d43d4ee90
mklingen <mklingen89@gmail.com>
2020-02-02 12:02:35 -0500

    shadows

---
+++ Assets/Shadow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+    private float _initY = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _initY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, _initY, transform.position.z);
+    }
+}
